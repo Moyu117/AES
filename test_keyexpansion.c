27ff08b1f7647889a50b718b8122472b993b7c10
zhonggu-chen <zhongguchen6@gmail.com>
2025-03-25 08:20:01 +0100

    Update test_keyexpansion.c

--- test_keyexpansion.c
+++ test_keyexpansion.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include "aes.h"
 
-void print_word(unsigned char w[4][NB*(NR +1)], int i) {
-    printf("%02x%02x%02x%02x", w[0][i], w[1][i], w[2][i], w[3][i]);
+void print_word(int word) {
+    // 使用 %08x 确保输出 8 位十六进制
+    printf("%08x", word & 0xFFFFFFFF); // 确保无符号输出
 }
 
 int main() {
@@ -14,7 +15,7 @@ int main() {
     };
     int nk = 4;
     int nr = 10; // AES-128
-    unsigned char w[4][NB * (NR + 1)];
+    int w[NB * (NR + 1)];
 
     // 执行密钥扩展
     key_expansion(key, w, nk);
@@ -23,23 +24,20 @@ int main() {
     printf("Key Expansion Test (Nk = 4):\n");
     for (int i = 0; i <= 4 * nr + 3; i++) {
         printf("w[%d] = ", i);
-        print_word(w,i);
+        print_word(w[i]);
         printf("\n");
     }
 
     // 验证 w[4] 到 w[7]
-    unsigned char expected_w4[4] = {0xa0, 0xfa, 0xfe, 0x17};
-    unsigned char expected_w5[4] = {0x88, 0x54, 0x2c, 0xb1};
-    unsigned char expected_w6[4] = {0x23, 0xa3, 0x39, 0x39};
-    unsigned char expected_w7[4] = {0x2a, 0x6c, 0x76, 0x05};
+    int expected_w4 = 0xa0fafe17;
+    int expected_w5 = 0x88542cb1;
+    int expected_w6 = 0x23a33939;
+    int expected_w7 = 0x2a6c7605;
 
     int pass = 1;
-    for (int j = 0; j < 4; j++) {
-        if (w[j][4] != expected_w4[j] || w[j][5] != expected_w5[j] ||
-            w[j][6] != expected_w6[j] || w[j][7] != expected_w7[j]) {
-            pass = 0;
-            break;
-        }
+    if (w[4] != expected_w4 || w[5] != expected_w5 ||
+        w[6] != expected_w6 || w[7] != expected_w7) {
+        pass = 0;
     }
 
     if (pass) {
