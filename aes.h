43d85ef3a7d0794c852db94a723d1c85b9bf6df2
zhonggu-chen <zhongguchen6@gmail.com>
2025-03-25 08:19:42 +0100

    Update aes.h

--- aes.h
+++ aes.h
@@ -1,13 +1,14 @@
+// aes.h
 #ifndef AES_H
 #define AES_H
 #define NB 4
 #define NR 14 // 最大轮数（256 位密钥）
 
-void key_expansion(unsigned char *key, unsigned char w[4][NB * (NR + 1)], int nk);
-void key_expansion_eic(unsigned char *key, unsigned char dw[4][NB * (NR + 1)], int nk);
-void cipher(unsigned char *in, unsigned char *out, int nr, unsigned char w[4][NB * (NR + 1)]);
-void inv_cipher(unsigned char *in, unsigned char *out, int nr, unsigned char w[4][NB * (NR + 1)]);
-void eq_inv_cipher(unsigned char *in, unsigned char *out, int nr, unsigned char dw[4][NB * (NR + 1)]);
+void key_expansion(unsigned char *key, int w[NB * (NR + 1)], int nk);
+void key_expansion_eic(unsigned char *key, int dw[NB * (NR + 1)], int nk);
+void cipher(unsigned char *in, unsigned char *out, int nr, int w[NB * (NR + 1)]);
+void inv_cipher(unsigned char *in, unsigned char *out, int nr, int w[NB * (NR + 1)]);
+void eq_inv_cipher(unsigned char *in, unsigned char *out, int nr, int dw[NB * (NR + 1)]);
 void aes_encrypt(unsigned char *in, unsigned char *key, unsigned char *out, int nk);
 void aes_decrypt(unsigned char *in, unsigned char *key, unsigned char *out, int nk);
 void aes_eq_decrypt(unsigned char *in, unsigned char *key, unsigned char *out, int nk);
