4aaf064286f69ef9caf12446bc2d3b357f07ade8
zhonggu-chen <zhongguchen6@gmail.com>
2025-04-01 12:31:47 +0200

    Update modes.h

--- modes.h
+++ modes.h
@@ -2,10 +2,17 @@
 #ifndef MODES_H
 #define MODES_H
 
-void ecb_encrypt(unsigned char *plaintext, unsigned char *key, unsigned char *ciphertext, int len, int nk);
-void ecb_decrypt(unsigned char *ciphertext, unsigned char *key, unsigned char *plaintext, int len, int nk, int use_eq);
-void cbc_encrypt(unsigned char *plaintext, unsigned char *key, unsigned char *iv, unsigned char *ciphertext, int len, int nk);
-void cbc_decrypt(unsigned char *ciphertext, unsigned char *key, unsigned char *iv, unsigned char *plaintext, int len, int nk, int use_eq);
-void cmac(unsigned char *message, unsigned char *key, unsigned char *mac, int len, int nk);
+#define NB 4
+int nk;
+unsigned int w[60];
+
+void ecb_encrypt(unsigned char *in, unsigned char *out, int len);
+void ecb_decrypt(unsigned char *in, unsigned char *out, int len);
+void cbc_encrypt(unsigned char *in, unsigned char *out, int len, unsigned char *iv);
+void cbc_decrypt(unsigned char *in, unsigned char *out, int len, unsigned char *iv);
+void cfb_encrypt(unsigned char *in, unsigned char *out, int len, unsigned char *iv);
+void cfb_decrypt(unsigned char *in, unsigned char *out, int len, unsigned char *iv);
+void ofb_encrypt_decrypt(unsigned char *in, unsigned char *out, int len, unsigned char *iv);
+int compare_blocks(unsigned char *block1, unsigned char *block2);
 
 #endif
